4c250f8e7654696fe345264e6ac45119f316d61f
Arun Chandran <arun@spawoz.com>
2017-08-30 22:32:19 +0530

    TYPO3 8.7 Compatible Fixes

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.0',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.5.0',
+			'typo3' => '6.2.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
--- ext_tables.php
+++ ext_tables.php
@@ -13,29 +13,3 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pepagephpcontentelement_domain_model_phpcontent', 'EXT:pe_pagephpcontentelement/Resources/Private/Language/locallang_csh_tx_pepagephpcontentelement_domain_model_phpcontent.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pepagephpcontentelement_domain_model_phpcontent');
-$GLOBALS['TCA']['tx_pepagephpcontentelement_domain_model_phpcontent'] = array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:pe_pagephpcontentelement/Resources/Private/Language/locallang_db.xlf:tx_pepagephpcontentelement_domain_model_phpcontent',
-		'label' => 'title',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => TRUE,
-
-		'versioningWS' => 2,
-		'versioning_followPages' => TRUE,
-
-		'languageField' => 'sys_language_uid',
-		'transOrigPointerField' => 'l10n_parent',
-		'transOrigDiffSourceField' => 'l10n_diffsource',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'searchFields' => 'title,php_content,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/PHPContent.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_pepagephpcontentelement_domain_model_phpcontent.gif'
-	),
-);
